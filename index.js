0572e4d693d2913a28e90e894c22df06967b6f7f
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-05-17 16:46:42 +0300

    task3-js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import './js/task-1.js';
- import './js/task-2.js';
-// import './js/task-3.js';
+ //import './js/task-2.js';
+ import './js/task-3.js';
 // import './js/task-4.js';
 // import './js/task-5.js';
 // import './js/task-6.js';
