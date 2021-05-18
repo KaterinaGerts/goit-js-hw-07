2c1cbc22e4ce2798e8a02dd83d8333c177fda162
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-05-18 10:58:24 +0300

    task-4.js

---
+++ js/task-4.js
@@ -0,0 +1,35 @@
+// Счетчик состоит из спана и кнопок,
+// которые должны увеличивать и уменьшать значение
+// счетчика на 1.
+
+// Создай переменную counterValue в которой будет
+// хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения
+// и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций
+// и обновление интерфейса
+
+const incrementBtn = document.querySelector('[data-action="increment"]');
+// console.log(incrementBtn);
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+// console.log(decrementBtn);
+
+let counterValue = 0;
+
+const increment = () => {
+  counterValue += 1;
+  document.querySelector('#value').textContent = counterValue;
+}
+increment();
+
+
+const decrement = () => {
+  counterValue -= 1;
+  document.querySelector('#value').textContent = counterValue;
+}
+decrement();
+
+
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
+
